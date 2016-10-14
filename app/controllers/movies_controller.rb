21b4fa92a4abb3a3423039ea260ebe293878c022
Kayla Weaver <kweave26@uncc.edu>
2016-10-14 02:17:41 +0000

    Create the new controller and view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,5 +7,9 @@ class MoviesController < ApplicationController
   id = params[:id] # retrieve movie ID from URI route
   @movie = Movie.find(id) # look up movie by unique ID
   # will render app/views/movies/show.html.haml by default
-    end
+  end
+  def new
+  @movie = Movie.new
+  # default: render 'new' template
+  end 
 end
